a1384b8aa07506124e9a3413eee4e01548d2aa71
VikrantKS <96419531+VikrantKS@users.noreply.github.com>
2021-12-30 11:24:06 +0530

    added dummy test suite

---
+++ test/dummy.spec.ts
@@ -0,0 +1,16 @@
+import { padRight, isWindows } from '../src/util'
+import { expect } from 'chai'
+import chalk from 'chalk'
+import { getTarget, NexeTarget } from '../src/target'
+const b = chalk.blue
+const arch = process.arch === 'ia32' ? 'x86' : process.arch
+
+describe('dummy', () => {
+  ;
+  
+  it('Dummy-test-suite-1', () => {
+    
+    expect(JSON.stringify(getTarget(process))).to.equal(`"${getTarget(process)}"`)
+  })
+  
+})
