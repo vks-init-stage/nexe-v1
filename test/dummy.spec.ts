59bd1a4897e975f0578b3e384b251f0dfe60590b
VikrantKS <96419531+VikrantKS@users.noreply.github.com>
2022-02-14 16:11:59 +0530

    Update dummy.spec.ts

--- test/dummy.spec.ts
+++ test/dummy.spec.ts
@@ -12,7 +12,6 @@ describe('dummy', () => {
     expect(JSON.stringify(getTarget(process))).to.equal(`"${getTarget(process)}"`)
   })
   
-  
    it('Dummy-test-suite-2', () => {
     
     expect(JSON.stringify(getTarget(process))).to.equal(`"${getTarget(process)}"`)
